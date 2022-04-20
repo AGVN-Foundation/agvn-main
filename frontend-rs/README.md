8d6439657f5427c567793c61e350332f702bbfb6
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-04-21 07:27:48 +1000

    Add stuff

---
+++ frontend-rs/README.md
@@ -0,0 +1,3 @@
+# Frontend
+
+Built in rust. But doesnt even work on m1 mac it seems. With default target and `wasm32/64-unknown-unknown`. None works.
---
+++ frontend-rs/src/main.rs
@@ -0,0 +1,15 @@
+use dioxus::prelude::*;
+
+fn app(cx: Scope) -> Element {
+    let mut count = use_state(&cx, || 0);
+
+    cx.render(rsx! {
+        h1 { "High-Five counter: {count}" }
+        button { onclick: move |_| count += 1, "Up high!" }
+        button { onclick: move |_| count -= 1, "Down low!" }
+    })
+}
+
+fn main() {
+    dioxus::web::launch(app);
+}
