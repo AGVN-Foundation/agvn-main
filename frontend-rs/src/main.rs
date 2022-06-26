51797bcbb938fdc46f58581b5feedf077088a2cf
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-06-27 06:03:39 +1000

    Add struff

--- frontend-rs/src/main.rs
+++ frontend-rs/src/main.rs
@@ -1,13 +1,30 @@
 use dioxus::prelude::{dioxus_elements::div, *};
 
-fn app(cx: Scope) -> Element {
+pub fn app(cx: Scope) -> Element {
     let mut count = use_state(&cx, || 0);
+
+    let container_style = r#"
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    "#;
+
+    let rect_style = r#"
+        background: deepskyblue;
+        height: 50vh;
+        width: 50vw;
+    "#;
 
     cx.render(rsx! {
         h1 { "High-Five counter: {count}" }
         button { onclick: move |_| count += 1, "Up high!" }
         button { onclick: move |_| count -= 1, "Down low!" }
         div {"Hi"}
+        div {
+            style: "{container_style}",
+            "Hi",
+            frontend_rs::chakra::example {}
+        }
     })
 }
 
