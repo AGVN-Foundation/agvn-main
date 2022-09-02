0445eb890348394d531cc4a23d0a3622264be0ea
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-09-02 10:08:29 +1000

    Ok

--- hyperml/src/main.rs
+++ hyperml/src/main.rs
@@ -1,5 +1,18 @@
+use polars::prelude::*;
+use polars_io::prelude::*;
+use std::fs::File;
+
 fn main() {
     println!("Hello, world!");
+
+    let data = CsvReader::from_path("examples/datasets/world-population.csv")
+        .unwrap()
+        .has_header(true)
+        .finish()
+        .unwrap();
+
+    let head = data.head(Some(5));
+    println!("head = {head}");
 }
 
-fn input_data() {}
+fn input_data(data: DataFrame) {}
